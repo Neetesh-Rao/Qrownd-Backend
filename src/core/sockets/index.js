3e65e9757d4fa92dcd0de4fc6caba42f7e7ddce2
Neetesh Kumar Yadav <neeteshyadav0206@gmail.com>
2026-04-06 10:57:13 +0530

    vrrercrl

--- src/core/sockets/index.js
+++ src/core/sockets/index.js
@@ -20,7 +20,7 @@ let ioInstance = null
 const initSockets = (httpServer) => {
   ioInstance = new Server(httpServer, {
     cors: {
-      origin:      process.env.FRONTEND_URL || 'http://localhost:3000',
+      origin:      'https://qrownd-frontend.vercel.app',
       methods:     ['GET','POST'],
       credentials: true,
     },
